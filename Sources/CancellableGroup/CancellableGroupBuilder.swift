063b5ea1d01f163983770d9c8b056318b57f7ef8
Christopher Weems <hello@christopherweems>
2021-03-09 15:22:40 -0500

    Add `CancellableGroupBuilder.buildExpression(..)` for `ForEach` struct

--- Sources/CancellableGroup/CancellableGroupBuilder.swift
+++ Sources/CancellableGroup/CancellableGroupBuilder.swift
@@ -2,6 +2,7 @@
 //  Created by Christopher Weems on 2/22/21
 
 import Combine
+import ForEach
 
 @resultBuilder
 public struct CancellableGroupBuilder {
@@ -34,8 +35,25 @@ extension CancellableGroupBuilder {
         cancellable
     }
     
-    public static func buildExpression(_ expression: Optional<Cancellable>) -> Cancellable {
-        expression ?? AnyCancellable { }
+    public static func buildExpression(_ cancellable: Optional<Cancellable>) -> Cancellable {
+        cancellable ?? AnyCancellable { }
+    }
+    
+    public static func buildExpression<Data, Content>(_ forEach: ForEach<Data, Content>) -> Cancellable
+        where Content : Cancellable {
+        let content = forEach.data.map(forEach.content)
+        
+        return AnyCancellable {
+            content.cancel()
+            
+        }
+    }
+    
+}
+
+fileprivate extension Collection where Element : Cancellable {
+    func cancel() {
+        self.forEach { $0.cancel() }
     }
     
 }
