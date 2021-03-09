c526bf23efcdbd492c96d317bd4b0885e7da58ff
Christopher Weems <hello@christopherweems>
2021-03-09 15:21:19 -0500

    Add `ForEach` package dependency

--- Package.swift
+++ Package.swift
@@ -19,13 +19,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/christopherweems/ForEach.git", .upToNextMajor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CancellableGroup",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ForEach", package: "ForEach"),
+            ]),
         .testTarget(
             name: "CancellableGroupTests",
             dependencies: ["CancellableGroup"]),
